cc531fc9bebdf2276876406d2c97921d48e23e45
Nishant Kumar Prasad <45433024+iamnishantk@users.noreply.github.com>
2025-09-08 12:04:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     plugins: [vue()],
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+   base:'/The-Digital-Agronomist---Crop-Doctor-AI/',
 });
