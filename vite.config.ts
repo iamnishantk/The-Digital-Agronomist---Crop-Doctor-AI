7e3ede59e7c0f3f5295f07e2928b7b0f836c72e9
Nishant Kumar Prasad <45433024+iamnishantk@users.noreply.github.com>
2025-09-08 11:59:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    plugins: [vue()],
     base:'/The-Digital-Agronomist---Crop-Doctor-AI';
     return {
       define: {
