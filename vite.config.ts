e0038845c3c9814e6b5e2524db19edf352d48fb8
Nishant Kumar Prasad <45433024+iamnishantk@users.noreply.github.com>
2025-09-08 12:06:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-
+import vue from '@vitejs/plugin-vue';
+ 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     plugins: [vue()],
